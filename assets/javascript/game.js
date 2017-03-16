85256922471335339a2f01a665abaaa6138d2de8
walidpitt <walidpitt@gmail.com>
2017-03-16 12:28:16 -0400

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,10 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 var crystalValue;
-var timerStarted = false;
 var counter;
 
-// Timer object for game
-var timer = {
-  time:20,
-  reset: function(){
-    timer.time = 20;
-    $('#timer').html('Time remaining: ' + timer.time);
-    clearInterval(counter);
-    timer.start();
-  },
-  start: function(){
-    counter = setInterval(timer.count,1000);
-  },
-  stop: function(){
-    clearInterval(counter);
-  },
-  count: function(){
-    timer.time--;
-    $('#timer').html('Time remaining: ' + timer.time);
 
-    if(timer.time == 0){
-      timer.stop();
-      outOfTime();
-    }
-  }
-};
 
-// Changes crystal values to 4 new different random numbers
 function changeCrystalValues(){
   crystalValue = [];
   for(var i = 0; i < 4; i++){
@@ -45,7 +19,7 @@ function changeCrystalValues(){
 
 changeCrystalValues();
 
-// Initial display
+
 $("#random-number").html(randomNumber);
 $("#wins").html("Wins<br>" + wins);
 $("#losses").html("Losses<br>" + losses);
@@ -53,12 +27,9 @@ $("#score").html(score);
 
 $(".crystal").on("click",function(){
 
-  if(!timerStarted){
-    timer.reset();
-    timerStarted = true;
-  }
+  
 
-  // Update score depending on crystal pressed
+  
   if(this.value == "0"){
     score += crystalValue[0];
     $("#score").html(score);
@@ -76,49 +47,40 @@ $(".crystal").on("click",function(){
     $("#score").html(score);
   }
 
-  // If user loses...
+ 
   if(score > randomNumber){
     userLoses();
-    timer.stop();
-    timerStarted = false;
   }
 
-  // If user wins...
+  
   else if(score == randomNumber){
     wins++;
 
-    // Reset variables  
+     
     randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     score = 0;
     changeCrystalValues();
 
-    // Update display
+    
     $("#random-number").html(randomNumber);
     $("#wins").html("Wins<br>" + wins + "<br><br>You won!");
     $("#losses").html("Losses<br>" + losses);
     $("#score").html(score);
 
-    timer.stop();
-    timerStarted = false;
+    
   }
 });
 
-// Function is called when the timer hits zero
-function outOfTime(){
-  userLoses();
-  timer.stop();
-  timerStarted = false;
-}
+
 
 function userLoses(){
   losses++;
 
-  // Reset variables  
+   
   randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
   score = 0;
   changeCrystalValues();
 
-  // Update display
   $("#random-number").html(randomNumber);
   $("#wins").html("Wins<br>" + wins);
   $("#losses").html("Losses<br>" + losses + "<br><br>You lost!");
